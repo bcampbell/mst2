72442350da57670a3432727653fa48fcba45026b
Ben Campbell <ben@scumways.com>
2014-06-02 16:17:39 +1200

    fix up footer blurb and contact details

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
         <h3>Media Standards Trust</h3>
         <div class="l-stackable">
             <div class="l-item50">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue.</p>
+                <p>The Media Standards Trust is an independent registered charity that fosters quality, transparency and accountability for the digital age. We develop digital tools, conduct research, organise debates and contribute to public policy.</p>
                 <p>
                 Registered charity number 1113680<br/>
                 Company number 05514310    
@@ -13,13 +13,13 @@
 
             <div class="l-item25">
                 King's College London<br/>
-                Strand Campus<br/>
-                Strand Bridge House<br/>
-                London WC2R 1HH<br/>
+                Virginia Woolf Building<br/>
+                22 Kingsway<br/>
+                London WC2B 6NR<br/>
             </div>
 
             <div class="l-item25">
-                +44 (0) 207 848 7950
+                +44 20 7848 7950
             </div>
         </div>
     </div> <!-- end .l-contain -->
