e8b6a70672cd746f47a457fe5aaea667e742fd99
Ben Campbell <ben@scumways.com>
2015-01-27 02:06:43 +0000

    use php comments instead of html ones

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form class="searchform" action="/" method="get">
     <input placeholder="Search" type="search" name="s" value="<?php the_search_query(); ?>" />
        
-    <!-- <input type="image" alt="Search" class="submit" src="<?php bloginfo( 'template_url' ); ?>/img/searchicon.png" /> -->
+    <?php /* <input type="image" alt="Search" class="submit" src="<?php bloginfo( 'template_url' ); ?>/img/searchicon.png" /> */ ?>
        
 
 </form>
